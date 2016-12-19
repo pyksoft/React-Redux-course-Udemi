69a5b7a01f319f8915b3aa0c7de8a9d20fe0cd13
pyksoft <pyksoft@gmail.com>
2016-12-19 11:12:13 +0100

    created App class component from functional component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
-
 const API_KEY = 'AIzaSyCR4xZeUN2EPox-_Ra9nyrpdnfo-9JO2C8'
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data)
-})
 
 // Create a new component. This component should produce some HTML
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
-}
+class App extends Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {videos: [] }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+      console.log(data)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+}
 
 // After we created the component we put it on the page (DOM)
 
