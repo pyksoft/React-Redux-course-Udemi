098ef5abef2fdc1f2b076420a640d10e09a4c905
pyksoft <pyksoft@gmail.com>
2016-12-19 11:00:00 +0100

    working Youtube API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCR4xZeUN2EPox-_Ra9nyrpdnfo-9JO2C8'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data)
+})
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce some HTML
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+
+// After we created the component we put it on the page (DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'))
