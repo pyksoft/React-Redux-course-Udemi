f430b85d04c4fd3db3ed9edce526f4a0448f9640
pyksoft <pyksoft@gmail.com>
2016-12-22 11:43:47 +0100

    search function

--- src/index.js
+++ src/index.js
@@ -17,7 +17,11 @@ class App extends Component {
       selectedVideo: null
      }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -25,10 +29,13 @@ class App extends Component {
     })
   }
 
+
+
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
